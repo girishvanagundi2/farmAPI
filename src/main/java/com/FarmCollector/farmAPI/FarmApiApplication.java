975c66e8bfa96f3e1e98b810ad166142d60d093a
girishvanagundi2 <girishvanagundi2@gmail.com>
2024-11-09 14:26:27 +0530

    Initial Commit

---
+++ src/main/java/com/FarmCollector/farmAPI/FarmApiApplication.java
@@ -0,0 +1,13 @@
+package com.FarmCollector.farmAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FarmCollector/farmAPI/FarmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FarmCollector.farmAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
